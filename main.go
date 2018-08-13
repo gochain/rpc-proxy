fd99723bde18f1f0849e04c9dd14909bcc385b7c
Travis Reeder <treeder@gmail.com>
2018-08-13 13:43:36 -0700

    Trying x-requested-with
    https://stackoverflow.com/questions/16824661/cors-request-not-working-in-safari

--- main.go
+++ main.go
@@ -78,7 +78,7 @@ func main() {
 		cors := cors.New(cors.Options{
 			AllowedOrigins:   []string{"*"},
 			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
-			AllowedHeaders:   []string{"Origin", "Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+			AllowedHeaders:   []string{"Origin", "Accept", "Authorization", "Content-Type", "X-CSRF-Token", "x-requested-with"},
 			AllowCredentials: true,
 			MaxAge:           300, // Maximum value not ignored by any of major browsers
 		})
